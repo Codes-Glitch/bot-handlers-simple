bd8317112129235963f145a8c1b5ef740825e893
Glitch (bot-music-discord-js) <none>
2021-01-29 05:23:04 +0000

    🖐😻 Checkpoint ./server.js:26780611/6328 ./commands/Other/invite.js:26780611/5763

--- server.js
+++ server.js
@@ -3,17 +3,26 @@ const fs = require("fs");
 const db = require("wio.db");
 //const dbw = require("quick.db");
 const client = new Discord.Client();
-const { Default_Prefix, Token, Support, Color, Dashboard } = require("./config.js");
+const {
+  Default_Prefix,
+  Token,
+  Support,
+  Color,
+  Dashboard
+} = require("./config.js");
 client.commands = new Discord.Collection();
 client.aliases = new Discord.Collection();
 client.queue = new Map();
 
-
-
-client.on("ready", async () => {
-  console.log(`Bot Is Ready To Go!\nTag: ${client.user.tag}`);
-  client.user.setActivity(`Commands: mshelp \n Music With Members!\n ${client.guilds.cache.size} Server | ${client.users.cache.size} User
-   `, { type: "WATCHING" });
+client.on("ready", () => {
+  client.user.setStatus("idle");
+  client.user.setActivity(
+    `\nCommands: ${Default_Prefix}help\n${client.guilds.cache.size} Server | ${client.users.cache.size} User \nMade By FC 么 Glitch Editz `,
+    {
+      type: "WATCHING"
+    }
+  );
+  console.log(`Hi, ${client.user.username} is now online!`);
 });
 const { readdirSync } = require("fs");
 
@@ -25,33 +34,24 @@ let modules = ["./commands/../"];
     files.forEach(function(file) {
       if (!file.endsWith(".js"))*/
 
-  readdirSync("./commands/").forEach(dir => {
+readdirSync("./commands/").forEach(dir => {
+  // Filter so we only have .js command files
 
-        // Filter so we only have .js command files
+  const commands = readdirSync(`./commands/${dir}/`).filter(file =>
+    file.endsWith(".js")
+  );
 
-        const commands = readdirSync(`./commands/${dir}/`).filter(file => file.endsWith(".js"));
-
-    
-
-  
-        
-   // throw new Error(`A File Does Not End With .js!`);
-       for (let file of commands) {
-
-     
-      let command = require(`./commands/${module}/${file}`);
-      console.log(`${command.name} Has Been Loaded - ✅`);
-      if (command.name) client.commands.set(command.name, command);
-      if (command.aliases) {
-        command.aliases.forEach(alias =>
-          client.aliases.set(alias, command.name)
-        );
-      }
-      if (command.aliases.length === 0) command.aliases = null;
-    }});
-  ;
-;
-
+  // throw new Error(`A File Does Not End With .js!`);
+  for (let file of commands) {
+    let command = require(`./commands/${dir}/${file}`);
+    console.log(`${command.name} Has Been Loaded - ✅`);
+    if (command.name) client.commands.set(command.name, command);
+    if (command.aliases) {
+      command.aliases.forEach(alias => client.aliases.set(alias, command.name));
+    }
+    if (command.aliases.length === 0) command.aliases = null;
+  }
+});
 client.on("message", async message => {
   if (message.author.bot || !message.guild || message.webhookID) return;
 
@@ -65,16 +65,41 @@ client.on("message", async message => {
     .trim()
     .split(/ +/g);
   let cmd = args.shift().toLowerCase();
-  
+
   let command =
     client.commands.get(cmd) || client.commands.get(client.aliases.get(cmd));
-  
+
   if (!message.guild.me.hasPermission("SEND_MESSAGES")) return;
 
   if (!command)
     return message.channel.send(
       `No Command Found - ${cmd.charAt(0).toUpperCase() + cmd.slice(1)}`
     );
+  //const db = require("quick.db")
+
+const now = Date.now()
+
+if(db.has(`cd_${message.author.id}`)) {
+
+  const expirationTime = db.get(`cd_${message.author.id}`) + 3000
+
+  if(now < expirationTime) {
+
+  const timeLeft = (expirationTime - now) / 1000;
+
+		return message.reply(`<a:failed:798526823976796161> Please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${cmd}\` command.`);
+
+  }
+
+}
+
+  db.set(`cd_${message.author.id}`, now);
+
+  setTimeout(() => {
+
+    db.delete(`cd_${message.author.id}`)
+
+  },3000) 
 
   try {
     if (command) {
@@ -82,7 +107,11 @@ client.on("message", async message => {
     }
   } catch (error) {
     return message.channel.send(`Something Went Wrong, Try Again Later!`);
-  };
+  }
 });
 
-client.login(Token).catch(() => console.log(`Invalid Token Is Provided - Please Give Valid Token!`));
+client
+  .login(Token)
+  .catch(() =>
+    console.log(`Invalid Token Is Provided - Please Give Valid Token!`)
+  );
